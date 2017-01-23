e5cc1bf16631bb1f08b3fbb1b39ed3953bc67e54
Stephanie <sagorham@oakland.edu>
2017-01-23 15:43:18 -0500

    Added else for final verse, when x=1

--- BeerSong.java
+++ BeerSong.java
@@ -25,6 +25,12 @@ public class BeerSong {
              System.out.print( x + " bottles of beer,\ntake one down, pass it around, " + (x-1)+ " bottles of beer on the wall. \n");
             Ninety_Nine_Bottles_of_Beer(x-1); //decrement the x value
 }
+
+else if(x<=1){
+        System.out.println(x + " bottle of beer on the wall , "+ x + " bottle of beer\ntake one down, pass it around, no more bottles of beer on the wall"); 
+        
+      
+    }
 	
     }
 }
