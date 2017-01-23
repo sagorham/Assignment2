c3a1f890313146a9acec4cb8d88e44f90f8527e8
Stephanie <sagorham@oakland.edu>
2017-01-23 15:42:28 -0500

    Added if for verses 99-2

--- BeerSong.java
+++ BeerSong.java
@@ -15,12 +15,16 @@ public class BeerSong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      Ninety_Nine_Bottles_of_Beer(); 
+      Ninety_Nine_Bottles_of_Beer(99); 
     }
     
-    public static void Ninety_Nine_Bottles_of_Beer(){
+    public static void Ninety_Nine_Bottles_of_Beer(int x){
  
-        
+         if(x>1){
+            System.out.print(x +" bottles of beer on the wall ," ); 
+             System.out.print( x + " bottles of beer,\ntake one down, pass it around, " + (x-1)+ " bottles of beer on the wall. \n");
+            Ninety_Nine_Bottles_of_Beer(x-1); //decrement the x value
+}
 	
     }
 }
